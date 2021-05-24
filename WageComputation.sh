4e8ff190639bf65848c486eeffa4b904831fbee7
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-05-25 01:05:10 +0530

    Wages are calculated till days are less than 20 or hours less than 100

--- WageComputation.sh
+++ WageComputation.sh
@@ -2,18 +2,24 @@
 echo "Welcome to wage computation problem"
 wage=0
 wagePerHour=20
-for ((i=1;i<=20;i++))
+totalWorkHrs=0
+totalDays=0
+while [ $totalDays -lt 20  ] && [ $totalWorkHrs -lt 100 ]
 do
 	attendance=$((RANDOM %3))
 	case $attendance in
 		1)
 		echo "Employee is present"
 		hours=8
+		totalWorkHrs=$(($totalWorkHrs+8))
+		totalDays=$(($totalDays+1))
 		wage=$(($wage +($wagePerHour*$hours)))
 			;;
 		2)
 		echo "EMployee part time present"
 		hours=4
+		totalWorkHrs=$(($totalWorkHrs+4))
+		totalDays=$(($totalDays+1))
 		wage=$(($wage+($wagePerHour*$hours)))
 			;;
 		0)
@@ -22,4 +28,5 @@ do
 	esac
 done
 echo "monthly wage of emploeyee is "$wage
- 
+echo "Total hours worked are "$totalWorkHrs
+echo  "Total days worked are "  $totalDays
