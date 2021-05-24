06dc630643c6a01fb096b9473785af5d140220cd
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-05-25 00:48:55 +0530

    Calculates the montly wage of employees

--- WageComputation.sh
+++ WageComputation.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 echo "Welcome to wage computation problem"
+wage=0
 wagePerHour=20
-attendance=$((RANDOM %3))
-case $attendance
-	1)
-	echo "Employee is present"
-	hours=8
-	wage=$(($wagePerHour*$hours))
-	echo $wage
-		;;
-	2)
-	echo "EMployee part time present"
-	hours=4
-	wage=$(($wagePerHour*$hours))
-	echo $wage
-		;;
-	3)
-	echo "Employee is absent" 
-	echo "No wage as employee is absent"
-		;;
-esac
+for ((i=1;i<=20;i++))
+do
+	attendance=$((RANDOM %3))
+	case $attendance in
+		1)
+		echo "Employee is present"
+		hours=8
+		wage=$(($wage +($wagePerHour*$hours)))
+			;;
+		2)
+		echo "EMployee part time present"
+		hours=4
+		wage=$(($wage+($wagePerHour*$hours)))
+			;;
+		0)
+		echo "Employee is absent" 
+			;;
+	esac
+done
+echo "monthly wage of emploeyee is "$wage
+ 
