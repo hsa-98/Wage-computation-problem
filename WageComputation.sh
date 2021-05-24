2e3af0f4971f3d18c2d0eb4bbd77b6a4c7319c0f
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-05-25 00:33:24 +0530

    this adds part time wage

--- WageComputation.sh
+++ WageComputation.sh
@@ -2,11 +2,17 @@
 echo "Welcome to wage computation problem"
 wagePerHour=20
 
-attendance=$((RANDOM %2))
+attendance=$((RANDOM %3))
 if [ $attendance -eq 1 ]
 then
 	echo "Employee is present"
 	hours=8
+	wage=$(($wagePerHour*$hours))
+	echo $wage
+elif [ $attendance -eq 2 ]
+then	
+	echo "EMployee part time present"
+	hours=4
 	wage=$(($wagePerHour*$hours))
 	echo $wage
 else
