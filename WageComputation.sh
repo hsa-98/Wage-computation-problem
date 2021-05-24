9b8acc5325faee587b68b96ff4baf41fda8ca86b
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-05-25 00:12:29 +0530

    This will tell if an employee is present or absent

--- WageComputation.sh
+++ WageComputation.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "Welcome to wage computayion problem"
+echo "Welcome to wage computation problem"
+attendance=$((RANDOM %2))
+if [ $attendance -eq 1 ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent" 
+fi
