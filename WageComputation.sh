be454f45a867906ef4be15a86e756b1a4e1f2126
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-05-25 00:26:42 +0530

    This computes the wage of employee

--- WageComputation.sh
+++ WageComputation.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 echo "Welcome to wage computation problem"
+wagePerHour=20
+
 attendance=$((RANDOM %2))
 if [ $attendance -eq 1 ]
 then
 	echo "Employee is present"
+	hours=8
+	wage=$(($wagePerHour*$hours))
+	echo $wage
 else
 	echo "Employee is absent" 
+	echo "No wage as employee is absent"
 fi
