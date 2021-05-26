c7e1e5862a25e09519668d6af4dead15ee7736b0
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-05-26 22:38:39 +0530

    Refactors the code as a function

--- WageComputation.sh
+++ WageComputation.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
+function workHrs()
+{
+	case $1 in
+		1) echo 8
+			;;
+		2) echo 4
+			;;
+		0) echo 0
+			;;
+
+	esac
+}
 echo "Welcome to wage computation problem"
 wage=0
 wagePerHour=20
@@ -6,23 +18,25 @@ totalWorkHrs=0
 totalDays=0
 while [ $totalDays -lt 20  ] && [ $totalWorkHrs -lt 100 ]
 do
+	
 	attendance=$((RANDOM %3))
 	case $attendance in
 		1)
 		echo "Employee is present"
-		hours=8
+		hours=$(workHrs 1)
 		totalWorkHrs=$(($totalWorkHrs+8))
 		totalDays=$(($totalDays+1))
-		wage=$(($wage +($wagePerHour*$hours)))
+		wage=$(($wage +$wagePerHour * $hours))
 			;;
 		2)
 		echo "EMployee part time present"
-		hours=4
+		hours=$(workHrs 2)
 		totalWorkHrs=$(($totalWorkHrs+4))
 		totalDays=$(($totalDays+1))
-		wage=$(($wage+($wagePerHour*$hours)))
+		wage=$(($wage + $wagePerHour * $hours))
 			;;
 		0)
+		hours=$(workHrs 0)
 		echo "Employee is absent" 
 			;;
 	esac
